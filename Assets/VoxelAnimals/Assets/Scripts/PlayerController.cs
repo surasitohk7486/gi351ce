45556ea58d9a41eb0727d43bc688458a5fc0ce57
Nangil <surasit.dang@bumail.net>
2024-09-04 00:09:43 +0700

    Week1ce

---
+++ Assets/VoxelAnimals/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movementSpeed = 3;
+    public float jumpForce = 300;
+    public float timeBeforeNextJump = 1.2f;
+    private float canJump = 0f;
+    public float rotationSpeed;
+
+    [SerializeField]
+    private Transform cameraTransform;
+
+    Animator anim;
+    Rigidbody rb;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ControllPlayer();
+    }
+
+    void ControllPlayer()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        if (movement != Vector3.zero)
+        {
+            float targetAngle = Mathf.Atan2 (movement.z, movement.x) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            
+
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation,toRotation,rotationSpeed * Time.deltaTime);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            anim.SetInteger("Walk", 1);
+        }
+        else {
+            anim.SetInteger("Walk", 0);
+        }
+
+        movement = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) * movement;
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+
+        if (Input.GetButtonDown("Jump") && Time.time > canJump)
+        {
+                rb.AddForce(0, jumpForce, 0);
+                canJump = Time.time + timeBeforeNextJump;
+                anim.SetTrigger("jump");
+        }
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+
+    }
+}
